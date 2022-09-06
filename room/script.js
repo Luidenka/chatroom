3eb7d4a4cff373d2c026c9f0daaa03479432f43b
Luidenka <jarvislui1128@gmail.com>
2022-09-06 11:07:28 +0800

    Update script.js

--- room/script.js
+++ room/script.js
@@ -4,7 +4,7 @@ var input = document.getElementById('send_message');
 var socket = new WebSocket("wss://luidenka.github.io/chatroom/room");
 
 socket.onopen = (e) => {
-    addMessage('<div>Username have entered the chat room.</div>');
+    addMessage('<div>${username} have entered the chat room.</div>');
 };
 
 function addMessage(el) {
